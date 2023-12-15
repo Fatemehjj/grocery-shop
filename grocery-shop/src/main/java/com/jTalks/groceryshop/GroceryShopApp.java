3b251c023b3d1442c0dd07c30e7cc94504c44b73
fatemehjj <joolafatemeh652@gmail.com>
2023-12-15 23:57:55 +0330

    enable caching

--- grocery-shop/src/main/java/com/jTalks/groceryshop/GroceryShopApp.java
+++ grocery-shop/src/main/java/com/jTalks/groceryshop/GroceryShopApp.java
@@ -2,8 +2,10 @@ package com.jTalks.groceryshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GroceryShopApp {
         public static void main(String[] args) {
             SpringApplication.run(GroceryShopApp.class, args);
